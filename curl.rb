43f27983f16a6569d6517273f87b23a750bd7b09
haruki-kato <kuromitsu3719@gmail.com>
2019-11-19 13:09:47 +0900

    Update curl.rb

--- curl.rb
+++ curl.rb
@@ -4,7 +4,7 @@ require 'json'
 
 uri = URI.parse("http://<SonarQubeServerHostsName or ALB's DNS Name>/api/system/upgrades")
 request = Net::HTTP::Get.new(uri)
-request.basic_auth("admin", "admin")
+request.basic_auth("admin", "<AWS KMS 利用するなど>")
 
 json = Net::HTTP.get(uri)
 result = JSON.parse(json)
