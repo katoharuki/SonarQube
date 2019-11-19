db570ae63215699fba8ee2ef05a002302649435e
katoharuki <kuromitsu3719@gmail.com>
2019-11-19 04:07:47 +0000

    first commit

---
+++ curl.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI.parse("http://<SonarQubeServerHostsName or ALB's DNS Name>/api/system/upgrades")
+request = Net::HTTP::Get.new(uri)
+request.basic_auth("admin", "admin")
+
+json = Net::HTTP.get(uri)
+result = JSON.parse(json)
+
+(result["upgrades"]).count.times do |i|
+if (result["upgrades"][i]["description"]).include?("Long-Term Support version")
+  if i > 1
+    break
+  end
+    puts (result["upgrades"][i]["version"])
+  end
+end
